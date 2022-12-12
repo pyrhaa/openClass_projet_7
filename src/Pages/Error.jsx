193677a616a9b4a7d0c70af2a0265e337f1eedd1
pyrhaa <sboubtane@protonmail.com>
2022-12-12 20:18:02 +0100

    error page strusture ok

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,5 +1,13 @@
+import { Link } from 'react-router-dom';
+
 const Error = () => {
-  return <p>ⴰⵣⵓⵍ</p>;
+  return (
+    <div className="error">
+      <h1>404</h1>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/">Retourner sur la page d'accueil</Link>
+    </div>
+  );
 };
 
 export default Error;
