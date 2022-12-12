035489390402ce93f6d57a6002a7102c778168f7
pyrhaa <sboubtane@protonmail.com>
2022-12-12 20:54:30 +0100

    nav not finish

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,25 @@
 import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../assets/logos/NavbarLogo.svg';
 
 const Navbar = () => {
   return (
     <div>
-      <nav>
-        <Link to="/">Accueil</Link>
-        <Link to="/about">A propos</Link>
+      <Logo className="navbar__logo"></Logo>
+      <nav className="navbar__nav">
+        <Link
+          className={({ isActive }) =>
+            isActive ? 'navbar__nav__link active' : 'navbar__nav__link'
+          }
+          to="/">
+          Accueil
+        </Link>
+        <Link
+          className={({ isActive }) =>
+            isActive ? 'navbar__nav__link active' : 'navbar__nav__link'
+          }
+          to="/about">
+          A propos
+        </Link>
       </nav>
     </div>
   );
