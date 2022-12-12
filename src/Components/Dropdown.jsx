d009558b3a7eaa426fc40d6d727b1d1657850c18
pyrhaa <sboubtane@protonmail.com>
2022-12-13 00:22:05 +0100

    dropdown problems, something wrong

--- src/Components/Dropdown.jsx
+++ src/Components/Dropdown.jsx
@@ -1,5 +1,52 @@
+import { useState } from 'react';
 import Chevron from '../assets/icons/chevron.svg';
 
-const Dropdown = () => {};
+const Dropdown = ({ title, content }) => {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  const dropContent = () => {
+    if (typeof content === 'string') {
+      return <p>{content}</p>;
+    } else {
+      return (
+        <ul>
+          {content.map((item) => (
+            <li className="drop__list" key={item}>
+              {item}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  };
+
+  return (
+    <div className="drop">
+      <button
+        className="drop__visible"
+        onClick={() => {
+          toggleVisibility();
+        }}>
+        <h2 className="drop__visible__title">{title}</h2>
+        <img
+          src={Chevron}
+          alt="chevron down"
+          className={
+            visible ? 'drop__visible__chevron rotate' : 'drop__visible__chevron'
+          }></img>
+      </button>
+
+      <div
+        className={visible ? 'drop__toggle animated' : 'drop__toggle'}
+        style={{ height: visible ? `auto` : '0px' }}>
+        <div className="drop__toggle__content">{dropContent()}</div>
+      </div>
+    </div>
+  );
+};
 
 export default Dropdown;
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,18 +1,20 @@
 import about from '../data/about.json';
-import Hero from '../Components/Hero';
+// import Hero from '../Components/Hero';
 import Dropdown from '../Components/Dropdown';
 
 const About = ({ switcher }) => {
-  <div className="about">
-    <Hero origin="about" switcher={switcher('about')}></Hero>
-    <section className="about__guidelines">
-      {about.map((el) => {
-        return (
-          <Dropdown title={el.title} key={el.id} content={el.description} />
-        );
-      })}
-    </section>
-  </div>;
+  return (
+    <div className="about">
+      {/* <Hero origin="about" switcher={switcher('about')}></Hero> */}
+      <section className="about__guidelines">
+        {about.map((el) => {
+          return (
+            <Dropdown title={el.title} key={el.id} content={el.description} />
+          );
+        })}
+      </section>
+    </div>
+  );
 };
 
 export default About;
