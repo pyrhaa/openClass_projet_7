c3fb3456e2f2f97531843c31741e385be687bfe7
pyrhaa <sboubtane@protonmail.com>
2022-12-13 14:16:44 +0100

    error page style ok

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -3,9 +3,13 @@ import { Link } from 'react-router-dom';
 const Error = () => {
   return (
     <div className="error">
-      <h1>404</h1>
-      <p>Oups! La page que vous demandez n'existe pas.</p>
-      <Link to="/">Retourner sur la page d'accueil</Link>
+      <h1 className="error__title">404</h1>
+      <p className="error__description">
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+      <Link className="error__btn" to="/">
+        Retourner sur la page d'accueil
+      </Link>
     </div>
   );
 };
