6da94a5a7f935655ac584d12517805fde13b71b3
pyrhaa <sboubtane@protonmail.com>
2022-12-13 13:08:43 +0100

    rental page in progress, caroussel ok and dropdown in it too

--- src/Pages/Rental.jsx
+++ src/Pages/Rental.jsx
@@ -1,7 +1,23 @@
+import Carrousel from '../Components/Caroussel';
+import Dropdown from '../Components/Dropdown';
 import Error from './Error';
+import getData from '../data/data.json';
 
 const Rental = () => {
-  return <p>ⴰⵣⵓⵍ</p>;
+  const rentalData = getData;
+  const rentalUrlId = window.location.pathname.substring(8);
+  const rental = rentalData.find((item) => item.id === rentalUrlId);
+
+  if (!rental) return <Error />;
+  return (
+    <div className="rental">
+      <Carrousel pictures={rental.pictures}></Carrousel>
+      <div className="rental__details">
+        <Dropdown title="Équipements" content={rental.equipments}></Dropdown>
+        <Dropdown title="Description" content={rental.description}></Dropdown>
+      </div>
+    </div>
+  );
 };
 
 export default Rental;
